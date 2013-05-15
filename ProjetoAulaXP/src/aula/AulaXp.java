23e1ecdba20816391c6147aa47809af13762d098
maurolcsilva <maurolcsilva@gmail.com>
2013-05-14 21:22:23 -0300

    Criacao dos arquivos no GITHUB

---
+++ ProjetoAulaXP/src/aula/AulaXp.java
@@ -0,0 +1,13 @@
+package aula;
+
+public class AulaXp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testando o GitHub");
+	}
+
+}
